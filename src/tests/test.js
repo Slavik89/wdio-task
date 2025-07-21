5c00a501476c92a19166990572e18c52f37968ca
Yaroslav Kostiv <slovjans89@yahoo.com>
2025-07-21 07:43:08 +0300

    Small refactoring of the code was performed

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,12 +1,16 @@
 const { expect } = require('@wdio/globals')
 const loginPage = require('../po/login.page')
 const mainPage = require('../po/main.page')
-const { username, password } = require('./testData')
+const { username, password, expectedResultForTest1, expectedResultForTest2, expectedResultForTest3 } = require('./testData')
 
 describe('My Login application', () => {
 
-  beforeEach(async () => {    
+  beforeEach(async () => {   
+    
+    // 1. Go to Login page
     await loginPage.open();
+
+    // 2. Entering the username and password
     await loginPage.enterCredentials(username, password);
   });  
 
@@ -27,7 +31,7 @@ describe('My Login application', () => {
     const errorMessage = await loginPage.errorField.getText();
     
     // 1.7 Checking the main condition
-    expect(errorMessage).toContain('Username is required');
+    expect(errorMessage).toContain(expectedResultForTest1);
   });    
 
   it('Check the error messages: "Password is required", UC-2', async () => {
@@ -43,19 +47,19 @@ describe('My Login application', () => {
     const errorMessage = await loginPage.errorField.getText();
 
     // 2.6 Checking the main condition
-    expect(errorMessage).toContain('Password is required');
+    expect(errorMessage).toContain(expectedResultForTest2);
   });
 
   it("Check successful Login, UC-3", async () => {
 
-    // 3.2 Clicking the login button
+    // 3.3 Clicking the login button
     await loginPage.loginButton.click();
 
-    // 3.3 A variable to check successful login to the Main Page
+    // 3.4 A variable to check successful login to the Main Page
     const title = await mainPage.appTitle.getText();
 
-    // 3.4 Checking the main condition
-    expect(title).toBe('Swag Labs');
+    // 3.5 Checking the main condition
+    expect(title).toBe(expectedResultForTest3);
   });
   
 })
--- src/tests/testData.js
+++ src/tests/testData.js
@@ -1,5 +1,8 @@
 module.exports = {
 
   username: 'standard_user',
-  password: 'secret_sauce'
+  password: 'secret_sauce',
+  expectedResultForTest1: 'Username is required',
+  expectedResultForTest2: 'Password is required',
+  expectedResultForTest3: 'Swag Labs'
 };
